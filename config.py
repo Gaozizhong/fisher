6a0ba479a58abd0aff83806489b5c3cc4f2c10e2
Gaozizhong <631923319@qq.com>
2019-01-14 01:08:28 +0800

    加入配置文件

---
+++ config.py
@@ -0,0 +1,10 @@
+"""
+    @File  : config.py
+    @Author: GaoZizhong
+    @Date  : 2019/1/14
+    @Desc  : 
+"""
+
+__author__ = "GaoZizhong"
+
+DEBUG = True
--- fisher.py
+++ fisher.py
@@ -9,3 +9,12 @@ from flask import Flask
 __author__ = "GaoZizhong"
 
 app = Flask(__name__)
+app.config.from_object("config")
+
+
+@app.route("/hello")
+def hello():
+    return "hello gaozizhong"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=app.config["DEBUG"], port=81)
