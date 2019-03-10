2ea028cf60fadb30256250c552502902b2d670bc
Gaozizhong <631923319@qq.com>
2019-03-10 16:44:18 +0800

    用户注销完成

--- app/web/auth.py
+++ app/web/auth.py
@@ -5,7 +5,7 @@
     @Desc  :
 """
 from flask import render_template, request, redirect, url_for, flash
-from flask_login import login_user
+from flask_login import login_user, logout_user
 from app.forms.auth import RegisterForm, LoginForm
 from app.models.base import db
 from app.models.user import User
@@ -60,4 +60,5 @@ def change_password():
 
 @web.route('/logout')
 def logout():
-    pass
+    logout_user()
+    return redirect(url_for('web.index'))
