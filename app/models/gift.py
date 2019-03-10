00f191ca31244076ff311bf332b0f90c266251ea
Gaozizhong <631923319@qq.com>
2019-03-10 16:37:45 +0800

    礼物清单完成

--- app/models/gift.py
+++ app/models/gift.py
@@ -4,11 +4,14 @@
     @Date  : 2019/3/3 11:18
     @Desc  : 
 """
+from collections import namedtuple
+
 from flask import current_app
-from sqlalchemy import Column, Integer, Boolean, ForeignKey, String, desc
+from sqlalchemy import Column, Integer, Boolean, ForeignKey, String, desc, func
 from sqlalchemy.orm import relationship
 
-from app.models.base import Base
+from app.models.base import Base, db
+from app.models.wish import Wish
 from app.spider.yushu_book import YuShuBook
 
 __author__ = "GaoZizhong"
@@ -22,6 +25,22 @@ class Gift(Base):
     # book = relationship('Book')
     # bid = Column(Integer, ForeignKey('book.id'))
     launched = Column(Boolean, default=False)
+
+    @classmethod
+    def get_user_gifts(cls, uid):
+        gifts = Gift.query.filter_by(uid=uid, launched=False).order_by(
+            desc(Gift.creat_time)
+        ).all()
+        return gifts
+
+    @classmethod
+    def get_wish_count(cls, isbn_list):
+        # 根据传入的Isbn组，到Wish表中计算出某个礼物的数量
+        count_list = db.session.query(func.count(Wish.id), Wish.isbn).filter(Wish.launched == False,
+                                      Wish.isbn.in_(isbn_list),
+                                      Wish.status == 1).group_by(Wish.isbn).all()
+        count_list = [{'count': w(0), 'isbn': w[1]}for w in count_list]
+        return count_list
 
     @property
     def book(self):
