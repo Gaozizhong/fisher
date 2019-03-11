fd425b99855fe4e6b8b59c9a16841e51d24b2abe
Gaozizhong <631923319@qq.com>
2019-03-11 13:16:15 +0800

    鱼漂完成

--- app/web/drift.py
+++ app/web/drift.py
@@ -5,6 +5,7 @@
     @Desc  :
 """
 from flask import flash, redirect, url_for, render_template, request
+from sqlalchemy import desc, or_
 
 from app.forms.book import DriftForm
 from app.libs.email import send_mail
@@ -12,6 +13,7 @@ from app.models.base import db
 from app.models.drift import Drift
 from app.models.gift import Gift
 from app.view_models.book import BookViewModel
+from app.view_models.drift import DriftCollection
 from . import web
 from flask_login import login_required, current_user
 
@@ -21,8 +23,11 @@ __author__ = "GaoZizhong"
 @web.route('/drift/<int:gid>', methods=['GET', 'POST'])
 @login_required
 def send_drift(gid):
+    # filter_by(id=gid, launched=True)没有下面这条语句好。下面的语句可以多判断一种状态
     current_gift = Gift.query.get_or_404(gid)
-
+    # if current_gift.launched:
+    #     flash('这本书正处于交易状态，暂时不可以索要')
+    #     return redirect(url_for('web.book_detail', isbn=current_gift.isbn))
     if current_gift.is_yourself_gift(current_user.id):
         flash('这本书是你自己的^_^,不能向自己索要书籍哦')
         return redirect(url_for('web.book_detail', isbn=current_gift.isbn))
@@ -30,20 +35,28 @@ def send_drift(gid):
     if not can:
         return render_template('not_enough_beans.html', beans=current_user.beans)
 
-    # form = DriftForm(request.form)
-    # if request.method == 'POST' and form.validate():
-    #     save_drift(form, current_gift)
-    #     send_mail(current_gift.user.email, '有人想要一本书', 'email/get_gift.html',
-    #               wisher=current_user,
-    #               gift=current_gift)
+    form = DriftForm(request.form)
+    if request.method == 'POST' and form.validate():
+        save_drift(form, current_gift)
+        send_mail(current_gift.user.email, '有人想要一本书', 'email/get_gift.html',
+                  wisher=current_user,
+                  gift=current_gift)
 
     gifter = current_gift.user.summary
-    return render_template('drift.html', gifter=gifter, user_beans=current_user.beans)
+    return render_template('drift.html', gifter=gifter, user_beans=current_user.beans,
+                           form=form)
 
 
 @web.route('/pending')
+@login_required
 def pending():
-    pass
+    drifts = Drift.query.filter(
+        or_(Drift.requester_id == current_user.id,
+        Drift.gifter_id == current_user.id)
+    ).order_by(desc(Drift.creat_time)).all()
+
+    views = DriftCollection(drifts, current_user.id)
+    return render_template('pending.html', drifts=views.data)
 
 
 @web.route('/drift/<int:did>/reject')
